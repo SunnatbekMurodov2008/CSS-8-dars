958ac0906e219b82886c982ca643cf47c93dc08b
Muhammadali <murodovsunnatbek93@gmail.com>
2023-10-20 15:08:51 +0500

    CSS 8-dars
    27-guruh

---
+++ CSS 8-dars/js/main.js
@@ -0,0 +1,335 @@
+let wrap = document.getElementsByTagName('div').namedItem("wrap");
+let form_group = document.getElementsByTagName('div').namedItem("form-group");
+
+
+wrap.style.width ="1200px";
+wrap.style.margin="0 auto";
+
+form_group.style.width="500px";
+form_group.style.margin="o auto";
+form_group.style.boxShadow="0 0 10px rgba(80,80,80,0.5)";
+form_group.style.padding="1rem 1.2rem";
+form_group.style.boxSizing="border-box";
+
+let input1 = document.getElementsByTagName('input').namedItem("firstName");
+
+input1.style.width="60%";
+input1.style.padding="0.5rem 1.5rem";
+input1.style.boxSizing="border-box";
+input1.style.border="1px solid black";
+input1.style.borderRadius="4px";
+input1.style.display="inline-block";
+input1.style.marginBottom="10px";
+
+let input2 = document.getElementsByTagName('input').namedItem("lastName");
+
+input2.style.width="60%";
+input2.style.padding="0.5rem 1.5rem";
+input2.style.boxSizing="border-box";
+input2.style.border="1px solid black";
+input2.style.borderRadius="4px";
+input2.style.display="inline-block";
+input2.style.marginBottom="10px";
+
+let input3 = document.getElementsByTagName('input').namedItem("email");
+
+input3.style.width="60%";
+input3.style.padding="0.5rem 1.5rem";
+input3.style.boxSizing="border-box";
+input3.style.border="1px solid black";
+input3.style.borderRadius="4px";
+input3.style.display="inline-block";
+input3.style.marginBottom="10px";
+
+let input4 = document.getElementsByTagName('input').namedItem("premail");
+
+input4.style.width="60%";
+input4.style.padding="0.5rem 1.5rem";
+input4.style.boxSizing="border-box";
+input4.style.border="1px solid black";
+input4.style.borderRadius="4px";
+input4.style.display="inline-block";
+input4.style.marginBottom="10px";
+
+let input5 = document.getElementsByTagName('input').namedItem("password");
+
+input5.style.width="60%";
+input5.style.padding="0.5rem 1.5rem";
+input5.style.boxSizing="border-box";
+input5.style.border="1px solid black";
+input5.style.borderRadius="4px";
+input5.style.display="inline-block";
+input5.style.marginBottom="10px";
+
+let input6 = document.getElementsByTagName('input').namedItem("input6");
+
+input6.style.width="60%";
+input6.style.padding="0.5rem 1.5rem";
+input6.style.boxSizing="border-box";
+input6.style.border="1px solid black";
+input6.style.borderRadius="4px";
+input6.style.display="inline-block";
+input6.style.marginBottom="10px";
+
+let input7 = document.getElementsByTagName('input').namedItem("color");
+
+input7.style.width="60%";
+input7.style.padding="0.5rem 1.5rem";
+input7.style.boxSizing="border-box";
+input7.style.border="1px solid black";
+input7.style.borderRadius="4px";
+input7.style.display="inline-block";
+input7.style.marginBottom="10px";
+
+let input8 = document.getElementsByTagName('input').namedItem("week");
+
+input8.style.width="60%";
+input8.style.padding="0.5rem 1.5rem";
+input8.style.boxSizing="border-box";
+input8.style.border="1px solid black";
+input8.style.borderRadius="4px";
+input8.style.display="inline-block";
+input8.style.marginBottom="10px";
+
+let input9 = document.getElementsByTagName('input').namedItem("week");
+
+input9.style.width="60%";
+input9.style.padding="0.5rem 1.5rem";
+input9.style.boxSizing="border-box";
+input9.style.border="1px solid black";
+input9.style.borderRadius="4px";
+input9.style.display="inline-block";
+input9.style.marginBottom="10px";
+
+let input10 = document.getElementsByTagName('input').namedItem("month");
+
+input10.style.width="60%";
+input10.style.padding="0.5rem 1.5rem";
+input10.style.boxSizing="border-box";
+input10.style.border="1px solid black";
+input10.style.borderRadius="4px";
+input10.style.display="inline-block";
+input10.style.marginBottom="10px";
+
+let input11 = document.getElementsByTagName('input').namedItem("date");
+
+input11.style.width="60%";
+input11.style.padding="0.5rem 1.5rem";
+input11.style.boxSizing="border-box";
+input11.style.border="1px solid black";
+input11.style.borderRadius="4px";
+input11.style.display="inline-block";
+input11.style.marginBottom="10px";
+
+let input12 = document.getElementsByTagName('input').namedItem("time");
+
+input12.style.width="60%";
+input12.style.padding="0.5rem 1.5rem";
+input12.style.boxSizing="border-box";
+input12.style.border="1px solid black";
+input12.style.borderRadius="4px";
+input12.style.display="inline-block";
+input12.style.marginBottom="10px";
+
+
+let label1 = document.getElementsByTagName('label').namedItem("label1");
+
+label1.style.width="35%";
+label1.style.display="inline-block";
+label1.style.padding="0.5rem 1.5rem";
+label1.style.boxSizing="border-box";
+label1.style.marginBottom="10px";
+label1.style.textAlign="right";
+
+let label2 = document.getElementsByTagName('label').namedItem("label2");
+
+label2.style.width="35%";
+label2.style.display="inline-block";
+label2.style.padding="0.5rem 1.5rem";
+label2.style.boxSizing="border-box";
+label2.style.marginBottom="10px";
+label2.style.textAlign="right";
+
+let label3 = document.getElementsByTagName('label').namedItem("label3");
+
+label3.style.width="35%";
+label3.style.display="inline-block";
+label3.style.padding="0.5rem 1.5rem";
+label3.style.boxSizing="border-box";
+label3.style.marginBottom="10px";
+label3.style.textAlign="right";
+
+let label4 = document.getElementsByTagName('label').namedItem("label4");
+
+label4.style.width="35%";
+label4.style.display="inline-block";
+label4.style.padding="0.5rem 1.5rem";
+label4.style.boxSizing="border-box";
+label4.style.marginBottom="10px";
+label4.style.textAlign="right";
+
+let label5 = document.getElementsByTagName('label').namedItem("label5");
+
+label5.style.width="35%";
+label5.style.display="inline-block";
+label5.style.padding="0.5rem 1.5rem";
+label5.style.boxSizing="border-box";
+label5.style.marginBottom="10px";
+label5.style.textAlign="right";
+
+let label6 = document.getElementsByTagName('label').namedItem("label6");
+
+label6.style.width="35%";
+label6.style.display="inline-block";
+label6.style.padding="0.5rem 1.5rem";
+label6.style.boxSizing="border-box";
+label6.style.marginBottom="10px";
+label6.style.textAlign="right";
+
+let label8 = document.getElementsByTagName('label').namedItem("label8");
+
+label8.style.width="35%";
+label8.style.display="inline-block";
+label8.style.padding="0.5rem 1.5rem";
+label8.style.boxSizing="border-box";
+label8.style.marginBottom="10px";
+label8.style.textAlign="right";
+
+let label9 = document.getElementsByTagName('label').namedItem("label9");
+
+label9.style.width="35%";
+label9.style.display="inline-block";
+label9.style.padding="0.5rem 1.5rem";
+label9.style.boxSizing="border-box";
+label9.style.marginBottom="10px";
+label9.style.textAlign="right";
+
+let label10 = document.getElementsByTagName('label').namedItem("label10");
+
+label10.style.width="35%";
+label10.style.display="inline-block";
+label10.style.padding="0.5rem 1.5rem";
+label10.style.boxSizing="border-box";
+label10.style.marginBottom="10px";
+label10.style.textAlign="right";
+
+let label11 = document.getElementsByTagName('label').namedItem("label11");
+
+label11.style.width="35%";
+label11.style.display="inline-block";
+label11.style.padding="0.5rem 1.5rem";
+label11.style.boxSizing="border-box";
+label11.style.marginBottom="10px";
+label11.style.textAlign="right";
+
+let label12 = document.getElementsByTagName('label').namedItem("label12");
+
+label12.style.width="35%";
+label12.style.display="inline-block";
+label12.style.padding="0.5rem 1.5rem";
+label12.style.boxSizing="border-box";
+label12.style.marginBottom="10px";
+label12.style.textAlign="right";
+
+let label13 = document.getElementsByTagName('label').namedItem("label13");
+
+label13.style.width="35%";
+label13.style.display="inline-block";
+label13.style.padding="0.5rem 1.5rem";
+label13.style.boxSizing="border-box";
+label13.style.marginBottom="10px";
+label13.style.textAlign="right";
+
+let label14 = document.getElementsByTagName('label').namedItem("label14");
+
+label14.style.width="35%";
+label14.style.display="inline-block";
+label14.style.padding="0.5rem 1.5rem";
+label14.style.boxSizing="border-box";
+label14.style.marginBottom="10px";
+label14.style.textAlign="right";
+
+let label15 = document.getElementsByTagName('label').namedItem("label15");
+
+label15.style.width="35%";
+label15.style.display="inline-block";
+label15.style.padding="0.5rem 1.5rem";
+label15.style.boxSizing="border-box";
+label15.style.marginBottom="10px";
+label15.style.textAlign="right";
+
+let label16 = document.getElementsByTagName('label').namedItem("label16");
+
+label16.style.width="35%";
+label16.style.display="inline-block";
+label16.style.padding="0.5rem 1.5rem";
+label16.style.boxSizing="border-box";
+label16.style.marginBottom="10px";
+label16.style.textAlign="right";
+
+let label17 = document.getElementsByTagName('label').namedItem("label17");
+
+label17.style.width="35%";
+label17.style.display="inline-block";
+label17.style.padding="0.5rem 1.5rem";
+label17.style.boxSizing="border-box";
+label17.style.marginBottom="10px";
+label17.style.textAlign="right";
+
+let label18 = document.getElementsByTagName('label').namedItem("label18");
+
+label18.style.width="35%";
+label18.style.display="inline-block";
+label18.style.padding="0.5rem 1.5rem";
+label18.style.boxSizing="border-box";
+label18.style.marginBottom="10px";
+label18.style.textAlign="right";
+
+let label19 = document.getElementsByTagName('label').namedItem("label19");
+
+label19.style.width="35%";
+label19.style.display="inline-block";
+label19.style.padding="0.5rem 1.5rem";
+label19.style.boxSizing="border-box";
+label19.style.marginBottom="10px";
+label19.style.textAlign="right";
+
+let select1 = document.getElementsByTagName('select').namedItem("day");
+
+select1.style.width="20%";
+select1.style.display="inline-block";
+select1.style.borderRadius="4px";
+select1.style.border="1px solid #cccccc";
+select1.style.padding="0.5rem 1.5rem";
+
+let select2 = document.getElementsByTagName('select').namedItem("oy");
+
+select2.style.width="20%";
+select2.style.display="inline-block";
+select2.style.borderRadius="4px";
+select2.style.border="1px solid #cccccc";
+select2.style.padding="0.5rem 1.5rem";
+
+let select3 = document.getElementsByTagName('select').namedItem("year");
+
+select3.style.width="20%";
+select3.style.display="inline-block";
+select3.style.borderRadius="4px";
+select3.style.border="1px solid #cccccc";
+select3.style.padding="0.5rem 1.5rem";
+
+let select4 = document.getElementsByTagName('select').namedItem("oy");
+
+select2.style.width="20%";
+select2.style.display="inline-block";
+select2.style.borderRadius="4px";
+select2.style.border="1px solid #cccccc";
+select2.style.padding="0.5rem 1.5rem";
+
+let select5 = document.getElementsByTagName('select').namedItem("age");
+
+select5.style.width="20%";
+select5.style.display="inline-block";
+select5.style.borderRadius="4px";
+select5.style.border="1px solid #cccccc";
+select5.style.padding="0.5rem 1.5rem";
